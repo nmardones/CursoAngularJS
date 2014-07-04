58b332c858eb0db6396e25e660fc40803f48be1e
Alberto Basalo <albertobasalo@hotmail.com>
2014-07-04 07:47:26 +0200

    Parte 2

---
+++ 12-Directivas/client/ratingDirective.js
@@ -0,0 +1,31 @@
+angular.module('util-Rating', [])
+    .directive('myRating', function () {
+        return {
+            restrict: 'A',
+            templateUrl: '/starsTemplate.html' ,
+            scope: {
+                valor: '=',
+                max: '=',
+                readonly: '@'
+            },
+            link: function (scope, elem, attrs) {
+                var actualizar = function () {
+                    scope.stars = [];
+                    for (var i = 0; i < scope.max; i++) {
+                        scope.stars.push({
+                            marcada: i < scope.valor
+                        });
+                    }
+                };
+
+                scope.toggle = function (index) {
+                    if (scope.readonly && scope.readonly === 'true') {
+                        return;
+                    }
+                    scope.valor = index + 1;
+                    actualizar();                  
+                };
+                actualizar();
+            }
+        }
+    });
